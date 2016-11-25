0feb12aab9ee820ecf5bbbface8a3f44b51c175a
LoLo <luolc.witty@gmail.com>
2016-11-25 16:12:49 +0800

    Split the feature into two classes

---
+++ unibutton/src/main/java/com/luolc/unibutton/SimpleUniButton.java
@@ -0,0 +1,108 @@
+package com.luolc.unibutton;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
+import android.support.annotation.ColorInt;
+import android.support.v7.widget.AppCompatButton;
+import android.util.AttributeSet;
+
+/**
+ * @author LuoLiangchen
+ * @since 16/11/25
+ */
+
+public class SimpleUniButton extends AppCompatButton {
+
+    protected static final int[][] STATES;
+    protected static final int[] STATE_SET_DISABLED;
+    protected static final int[] STATE_SET_PRESSED;
+    protected static final int[] STATE_SET_FOCUSED;
+    protected static final int[] STATE_SET_NORMAL;
+
+    @ColorInt protected int mTextColor;
+    @ColorInt protected int mTextColorPressed;
+    @ColorInt protected int mTextColorDisabled;
+
+    protected StateListDrawable mBackground;
+    protected Drawable mBackgroundNormal;
+    protected Drawable mBackgroundPressed;
+    protected Drawable mBackgroundDisabled;
+
+    static {
+        STATE_SET_DISABLED = new int[] { -android.R.attr.state_enabled };
+        STATE_SET_PRESSED = new int[] { android.R.attr.state_enabled, android.R.attr.state_pressed };
+        STATE_SET_FOCUSED = new int[] { android.R.attr.state_enabled, android.R.attr.state_focused };
+        STATE_SET_NORMAL = new int[] { android.R.attr.state_enabled };
+        STATES = new int[][] {
+                STATE_SET_DISABLED,
+                STATE_SET_PRESSED,
+                STATE_SET_FOCUSED,
+                STATE_SET_NORMAL
+        };
+    }
+
+    public SimpleUniButton(Context context) {
+        this(context, null);
+    }
+
+    public SimpleUniButton(Context context, AttributeSet attrs) {
+        this(context, attrs, R.attr.buttonStyle);
+    }
+
+    public SimpleUniButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        if (!isInEditMode()) init(context, attrs);
+    }
+
+    protected void init(Context context, AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.SimpleUniButton);
+        initText(typedArray);
+        initBackground(typedArray);
+        typedArray.recycle();
+    }
+
+    protected void initText(TypedArray a) {
+        final ColorStateList textColorStateList = getTextColors();
+        @ColorInt final int currentTextColor = getCurrentTextColor();
+        @ColorInt final int defaultTextColor = textColorStateList.getColorForState(STATE_SET_NORMAL, currentTextColor);
+        @ColorInt final int defaultTextColorPressed = textColorStateList.getColorForState(STATE_SET_PRESSED, currentTextColor);
+        @ColorInt final int defaultTextColorDisabled = textColorStateList.getColorForState(STATE_SET_DISABLED, currentTextColor);
+        mTextColor = defaultTextColor;
+        mTextColorPressed = a.getColor(R.styleable.SimpleUniButton_textColorPressed, defaultTextColorPressed);
+        mTextColorDisabled = a.getColor(R.styleable.SimpleUniButton_textColorDisabled, defaultTextColorDisabled);
+        setTextColor(mTextColorDisabled, mTextColorPressed, mTextColorPressed, mTextColor);
+    }
+
+    protected void initBackground(TypedArray a) {
+        final Drawable currentBackgroundDrawable = getBackground();
+        if (currentBackgroundDrawable == null) {
+            throw new IllegalStateException("no background instance exist");
+        } else if (currentBackgroundDrawable instanceof StateListDrawable) {
+            mBackground = (StateListDrawable) currentBackgroundDrawable;
+            mBackgroundNormal = mBackground.getCurrent();
+        } else {
+            mBackground = new StateListDrawable();
+            mBackgroundNormal = currentBackgroundDrawable;
+        }
+
+        mBackgroundPressed = a.getDrawable(R.styleable.SimpleUniButton_backgroundPressed);
+        mBackgroundDisabled = a.getDrawable(R.styleable.SimpleUniButton_backgroundDisabled);
+        if (mBackgroundPressed == null) mBackgroundPressed = mBackgroundNormal;
+        if (mBackgroundDisabled == null) mBackgroundDisabled = mBackgroundNormal;
+
+        mBackground.addState(STATE_SET_DISABLED, mBackgroundDisabled);
+        mBackground.addState(STATE_SET_PRESSED, mBackgroundPressed);
+        mBackground.addState(STATE_SET_FOCUSED, mBackgroundPressed);
+        mBackground.addState(STATE_SET_NORMAL, mBackgroundNormal);
+
+        super.setBackgroundDrawable(mBackground);
+    }
+
+    private void setTextColor(@ColorInt int disabled, @ColorInt int pressed, @ColorInt int focused, @ColorInt int normal) {
+        @ColorInt final int[] colors = new int[] { disabled, pressed, focused, normal };
+        super.setTextColor(new ColorStateList(STATES, colors));
+    }
+}
--- unibutton/src/main/java/com/luolc/unibutton/UniButton.java
+++ unibutton/src/main/java/com/luolc/unibutton/UniButton.java
@@ -1,7 +1,6 @@
 package com.luolc.unibutton;
 
 import android.content.Context;
-import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
@@ -9,7 +8,6 @@ import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.StateListDrawable;
 import android.support.annotation.ColorInt;
 import android.support.annotation.Dimension;
-import android.support.v7.widget.AppCompatButton;
 import android.util.AttributeSet;
 
 /**
@@ -17,37 +15,9 @@ import android.util.AttributeSet;
  * @since 16/11/24
  */
 
-public class UniButton extends AppCompatButton {
-
-    protected static final int[][] STATES;
-    protected static final int[] STATE_SET_DISABLED;
-    protected static final int[] STATE_SET_PRESSED;
-    protected static final int[] STATE_SET_FOCUSED;
-    protected static final int[] STATE_SET_NORMAL;
-
-    @ColorInt protected int mTextColor;
-    @ColorInt protected int mTextColorPressed;
-    @ColorInt protected int mTextColorDisabled;
-
-    protected StateListDrawable mBackground;
-    protected GradientDrawable mBackgroundNormal;
-    protected GradientDrawable mBackgroundPressed;
-    protected GradientDrawable mBackgroundDisabled;
+public class UniButton extends SimpleUniButton {
 
     @Dimension protected int mRadius;
-
-    static {
-        STATE_SET_DISABLED = new int[] { -android.R.attr.state_enabled };
-        STATE_SET_PRESSED = new int[] { android.R.attr.state_enabled, android.R.attr.state_pressed };
-        STATE_SET_FOCUSED = new int[] { android.R.attr.state_enabled, android.R.attr.state_focused };
-        STATE_SET_NORMAL = new int[] { android.R.attr.state_enabled };
-        STATES = new int[][] {
-                STATE_SET_DISABLED,
-                STATE_SET_PRESSED,
-                STATE_SET_FOCUSED,
-                STATE_SET_NORMAL
-        };
-    }
 
     public UniButton(Context context) {
         this(context, null);
@@ -62,26 +32,15 @@ public class UniButton extends AppCompatButton {
         if (!isInEditMode()) init(context, attrs);
     }
 
-    private void init(Context context, AttributeSet attrs) {
+    @Override
+    protected void init(Context context, AttributeSet attrs) {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.UniButton);
         initText(typedArray);
         initBackground(typedArray);
-
         typedArray.recycle();
     }
 
-    protected void initText(TypedArray a) {
-        final ColorStateList textColorStateList = getTextColors();
-        @ColorInt final int currentTextColor = getCurrentTextColor();
-        @ColorInt final int defaultTextColor = textColorStateList.getColorForState(STATE_SET_NORMAL, currentTextColor);
-        @ColorInt final int defaultTextColorPressed = textColorStateList.getColorForState(STATE_SET_PRESSED, currentTextColor);
-        @ColorInt final int defaultTextColorDisabled = textColorStateList.getColorForState(STATE_SET_DISABLED, currentTextColor);
-        mTextColor = defaultTextColor;
-        mTextColorPressed = a.getColor(R.styleable.UniButton_textColorPressed, defaultTextColorPressed);
-        mTextColorDisabled = a.getColor(R.styleable.UniButton_textColorDisabled, defaultTextColorDisabled);
-        setTextColor(mTextColorDisabled, mTextColorPressed, mTextColorPressed, mTextColor);
-    }
-
+    @Override
     protected void initBackground(TypedArray a) {
         final Drawable currentBackgroundDrawable = getBackground();
         if (currentBackgroundDrawable == null) {
@@ -93,19 +52,23 @@ public class UniButton extends AppCompatButton {
         }
 
         @ColorInt final int backgroundColorNormal = ((ColorDrawable) currentBackgroundDrawable).getColor();
-        @ColorInt final int backgroundColorPressed = a.getColor(R.styleable.UniButton_backgroundPressed, backgroundColorNormal);
-        @ColorInt final int backgroundColorDisabled = a.getColor(R.styleable.UniButton_backgroundDisabled, backgroundColorNormal);
-        mBackgroundNormal = new GradientDrawable();
-        mBackgroundPressed = new GradientDrawable();
-        mBackgroundDisabled = new GradientDrawable();
-        mBackgroundNormal.setColor(backgroundColorNormal);
-        mBackgroundPressed.setColor(backgroundColorPressed);
-        mBackgroundDisabled.setColor(backgroundColorDisabled);
+        @ColorInt final int backgroundColorPressed = a.getColor(R.styleable.SimpleUniButton_backgroundPressed, backgroundColorNormal);
+        @ColorInt final int backgroundColorDisabled = a.getColor(R.styleable.SimpleUniButton_backgroundDisabled, backgroundColorNormal);
+        final GradientDrawable backgroundNormal = new GradientDrawable();
+        final GradientDrawable backgroundPressed = new GradientDrawable();
+        final GradientDrawable backgroundDisabled = new GradientDrawable();
+        backgroundNormal.setColor(backgroundColorNormal);
+        backgroundPressed.setColor(backgroundColorPressed);
+        backgroundDisabled.setColor(backgroundColorDisabled);
 
         mRadius = a.getDimensionPixelSize(R.styleable.UniButton_radius, 0);
-        mBackgroundNormal.setCornerRadius(mRadius);
-        mBackgroundPressed.setCornerRadius(mRadius);
-        mBackgroundDisabled.setCornerRadius(mRadius);
+        backgroundNormal.setCornerRadius(mRadius);
+        backgroundPressed.setCornerRadius(mRadius);
+        backgroundDisabled.setCornerRadius(mRadius);
+
+        mBackgroundNormal = backgroundNormal;
+        mBackgroundPressed = backgroundPressed;
+        mBackgroundDisabled = backgroundDisabled;
 
         mBackground.addState(STATE_SET_DISABLED, mBackgroundDisabled);
         mBackground.addState(STATE_SET_PRESSED, mBackgroundPressed);
@@ -113,10 +76,5 @@ public class UniButton extends AppCompatButton {
         mBackground.addState(STATE_SET_NORMAL, mBackgroundNormal);
 
         super.setBackgroundDrawable(mBackground);
-    }
-
-    private void setTextColor(@ColorInt int disabled, @ColorInt int pressed, @ColorInt int focused, @ColorInt int normal) {
-        @ColorInt final int[] colors = new int[] { disabled, pressed, focused, normal };
-        super.setTextColor(new ColorStateList(STATES, colors));
     }
 }
