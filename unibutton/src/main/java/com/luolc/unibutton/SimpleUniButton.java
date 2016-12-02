3926b460d085f65c55cbafc4330a79531f176766
LoLo <luolc.witty@gmail.com>
2016-12-02 17:03:44 +0800

    Add set background drawable from resId

--- unibutton/src/main/java/com/luolc/unibutton/SimpleUniButton.java
+++ unibutton/src/main/java/com/luolc/unibutton/SimpleUniButton.java
@@ -2,6 +2,8 @@ package com.luolc.unibutton;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 
 /**
@@ -27,11 +29,23 @@ public class SimpleUniButton extends AbstractUniButton {
         mBackgroundNormalWrapper.setWrappedDrawable(backgroundNormal);
     }
 
+    public void setBackgroundNormal(@DrawableRes int resId) {
+        mBackgroundNormalWrapper.setWrappedDrawable(ContextCompat.getDrawable(getContext(), resId));
+    }
+
     public void setBackgroundPressed(Drawable backgroundPressed) {
         mBackgroundPressedWrapper.setWrappedDrawable(backgroundPressed);
     }
 
+    public void setBackgroundPressed(@DrawableRes int resId) {
+        mBackgroundPressedWrapper.setWrappedDrawable(ContextCompat.getDrawable(getContext(), resId));
+    }
+
     public void setBackgroundDisabled(Drawable backgroundDisabled) {
         mBackgroundDisabledWrapper.setWrappedDrawable(backgroundDisabled);
+    }
+
+    public void setBackgroundDisabled(@DrawableRes int resId) {
+        mBackgroundDisabledWrapper.setWrappedDrawable(ContextCompat.getDrawable(getContext(), resId));
     }
 }
